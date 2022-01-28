06ed8d0ce6fc4e074912a616daf1208efdec27ed
Leoruiz197 <ruizruizleo@hotmail>
2022-01-27 21:54:31 -0300

    exe 5

--- Aula_08/Exercicios/exe5.js
+++ Aula_08/Exercicios/exe5.js
@@ -1,14 +1,10 @@
-// Imprimir os 10 primeiros números da sequência de Fibonacci sem usar recursividade 
-//Assim, a fórmula a seguir define a sequência: 
-//Fn = Fn - 1 + Fn - 2
+let fn = 0;
+let a = 0;
+let b = 1;
 
-let f0 = 0;
-let f1 = 1;
-console.log(f0);
-console.log(f1);
-for (let i = 0; i < 8; i++) {
-    let fi = f1 + f0;
-    console.log(fi);
-    f0 = f1;
-    f1 = fi;
+for (let k = 0; k < 10; k++) {
+  console.log(fn);
+  fn = b + a;
+  b = a;
+  a = fn;
 }
